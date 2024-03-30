bdcd1ff919712a3a3d40b0d0e334484b6a5b789d
BearKiller1 <71083628+BearKiller1@users.noreply.github.com>
2024-03-30 13:29:03 +0400

    db added

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
-    $host       = 'localhost';
-    $dbname     = 'lucid';
-    $username   = 'root';
-    $password   = '';
+
+    $host       = 'sql8.freemysqlhosting.net';
+    $dbname     = 'sql8695355';
+    $username   = 'sql8695355';
+    $password   = '4PPwnPs4I5';
 
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -10,4 +11,11 @@
     } catch (PDOException $e) {
         die("Database connection failed: " . $e->getMessage());
     }
+
+
+    // MySQL Database Name: gepeismonacemebi
+    // MySQL Username: sosocixisdzireli
+    // MySQL Database Name: gepeismonacemebi
+    // MySQL User Password: bali10shauri
+    // MySQL User Password Verification: bali10shauri
 ?>
